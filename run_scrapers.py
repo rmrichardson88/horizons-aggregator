bdc9d63de832c485d7a68082b552364d2130f15a
Ryan Richardson <116780532+rmrichardson88@users.noreply.github.com>
2025-08-09 14:45:06 -0600

    Update run_scrapers.py

--- run_scrapers.py
+++ run_scrapers.py
@@ -4,7 +4,7 @@ from utils import load_previous, save_latest
 
 SCRAPER_MODULES = [
     "scrapers.yhmc_board",
-    "scrapers.sage_oil_vac_board",
+    #"scrapers.sage_oil_vac_board",
     "scrapers.fmc_board"
     # "scrapers.other_board",
 ]
