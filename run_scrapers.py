f0e7d55038653999b7025fbd8f88fe80808853e1
Ryan Richardson <116780532+rmrichardson88@users.noreply.github.com>
2025-11-21 14:56:23 -0700

    Disable yhmc_board scraper
    Comment out the yhmc_board scraper in the list.

--- run_scrapers.py
+++ run_scrapers.py
@@ -7,7 +7,7 @@ SCRAPER_MODULES = [
     "scrapers.ttuhsc_board",
     "scrapers.fmc_board",
     "scrapers.anb_board",
-    "scrapers.yhmc_board",
+    #"scrapers.yhmc_board",
     #"scrapers.sage_oil_vac_board",
     "scrapers.talon_lpe_board"
     # "scrapers.other_board",
